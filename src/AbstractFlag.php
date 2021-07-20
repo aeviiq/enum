4d46fab0f05826fc4b4756c6640ad45f7e7b991f
kevins <k.van.sonsbeek@cak.nl>
2021-07-20 14:00:42 +0200

    Seperated the AbstractFlag from the Enum, due to the internal behaviour changing. With test.

--- src/AbstractFlag.php
+++ src/AbstractFlag.php
@@ -1,29 +1,44 @@
-<?php declare(strict_types = 1);
+<?php declare(strict_types=1);
 
 namespace Aeviiq\Enum;
 
 use Aeviiq\Enum\Exception\InvalidArgumentException;
-use MyCLabs\Enum\Enum;
 
-abstract class AbstractFlag extends Enum
+abstract class AbstractFlag
 {
     /**
-     * @var int[]
+     * @var int
+     */
+    protected $value;
+
+    /**
+     * @var array
+     * @psalm-var array<class-string, array<string, mixed>>
+     */
+    protected static $cache = [];
+
+    /**
+     * @var array<int>
      */
     protected static $flags = [];
 
-    public function __construct($value)
+    /**
+     * @throws \ReflectionException
+     */
+    public function __construct(int $value)
     {
         static::$flags[static::class] = \array_sum(static::toArray());
-        parent::__construct($value);
+
+        if (!static::isValid($value)) {
+            throw new \UnexpectedValueException(sprintf('Value \'%s\' is not part of the enum %s', $value, static::class));
+        }
+
+        $this->value = $value;
     }
 
-    /**
-     * @inheritdoc
-     */
-    public static function isValid($value): bool
+    public function getValue(): int
     {
-        return \is_int($value) ? ($value & static::$flags[static::class]) === $value : false;
+        return $this->value;
     }
 
     public function contains(AbstractFlag $flag): bool
@@ -32,11 +47,66 @@ abstract class AbstractFlag extends Enum
             throw new InvalidArgumentException(\sprintf('Argument 1 passed to %s() must be an instance of %s, %s given.', __METHOD__, static::class, \get_class($flag)));
         }
 
-        return $this->isFlagSet($this->getValue(), $flag->getValue());
+        $flagValue = $flag->getValue();
+
+        return ($this->getValue() & $flagValue) === $flagValue;
     }
 
-    protected function isFlagSet(int $flags, int $flag): bool
+    /**
+     * @return array<int>
+     */
+    public function explode(): array
     {
-        return ($flag & $flags) === $flag;
+        $values = [];
+
+        foreach (static::values() as $flag) {
+            if ($this->contains($flag)) {
+                $values[] = $flag->getValue();
+            }
+        }
+
+        return $values;
+    }
+
+    public function count(): int
+    {
+        return count($this->explode());
+    }
+
+    final public function equals(AbstractFlag $variable): bool
+    {
+        return $this->getValue() === $variable->getValue() && static::class === \get_class($variable);
+    }
+
+    /**
+     * @return array<string, static>
+     */
+    public static function values(): array
+    {
+        return array_map(static function ($value) {
+            return new static($value);
+        }, static::toArray());
+    }
+
+    /**
+     * @return array<string, int>
+     *
+     * @throws \ReflectionException
+     */
+    public static function toArray(): array
+    {
+        $class = static::class;
+
+        if (!isset(static::$cache[$class])) {
+            $reflection = new \ReflectionClass($class);
+            static::$cache[$class] = $reflection->getConstants();
+        }
+
+        return static::$cache[$class];
+    }
+
+    public static function isValid(int $value): bool
+    {
+        return ($value & static::$flags[static::class]) === $value;
     }
 }
