acd7ec342dd16c43e359eb52fef2392a8f6b1991
aeviiq <p.j.vanloon1991@gmail.com>
2023-12-12 15:23:43 +0100

    Update abstract flag and add unit tests for it

--- src/AbstractFlag.php
+++ src/AbstractFlag.php
@@ -1,39 +1,66 @@
-<?php declare(strict_types=1);
+<?php
+
+declare(strict_types=1);
 
 namespace Aeviiq\Enum;
 
 use Aeviiq\Enum\Exception\InvalidArgumentException;
+use Aeviiq\Enum\Exception\UnexpectedValueException;
 
 abstract class AbstractFlag
 {
     /**
-     * @var int
-     */
-    protected $value;
-
-    /**
      * @var array
      * @psalm-var array<class-string, array<string, int>>
      */
-    protected static $cache = [];
+    private static $cache = [];
 
     /**
      * @var array<int>
      */
-    protected static $flags = [];
+    private static $flags = [];
 
     /**
      * @throws \ReflectionException
      */
-    public function __construct(int $value)
+    final public function __construct(private readonly int $value)
     {
         static::$flags[static::class] = \array_sum(static::toArray());
 
         if (!static::isValid($value)) {
-            throw new \UnexpectedValueException(sprintf('Value \'%s\' is not part of the enum %s', $value, static::class));
+            throw new UnexpectedValueException(sprintf('Value \'%s\' is not part of the enum %s', $value, static::class));
+        }
+    }
+
+    /**
+     * @return array<string, static>
+     */
+    public static function values(): array
+    {
+        return \array_map(static function ($value) {
+            return new static($value);
+        }, static::toArray());
+    }
+
+    /**
+     * @return array<string, int>
+     *
+     * @throws \ReflectionException
+     */
+    public static function toArray(): array
+    {
+        $class = static::class;
+        if (!isset(static::$cache[$class])) {
+            $reflection = new \ReflectionClass($class);
+            static::$cache[$class] = $reflection->getConstants();
         }
 
-        $this->value = $value;
+        return static::$cache[$class];
+    }
+
+    public static function isValid(int $value): bool
+    {
+        return ($value & static::$flags[static::class]) === $value;
     }
 
     public function getValue(): int
@@ -76,37 +103,5 @@ abstract class AbstractFlag
     final public function equals(AbstractFlag $variable): bool
     {
         return $this->getValue() === $variable->getValue() && static::class === \get_class($variable);
-    }
-
-    /**
-     * @return array<string, static>
-     */
-    public static function values(): array
-    {
-        return \array_map(static function ($value) {
-            return new static($value);
-        }, static::toArray());
-    }
-
-    /**
-     * @return array<string, int>
-     *
-     * @throws \ReflectionException
-     */
-    public static function toArray(): array
-    {
-        $class = static::class;
-
-        if (!isset(static::$cache[$class])) {
-            $reflection = new \ReflectionClass($class);
-            static::$cache[$class] = $reflection->getConstants();
-        }
-
-        return static::$cache[$class];
-    }
-
-    public static function isValid(int $value): bool
-    {
-        return ($value & static::$flags[static::class]) === $value;
     }
 }
--- tests/AbstractFlagTest.php
+++ tests/AbstractFlagTest.php
@@ -1,27 +1,16 @@
-<?php declare(strict_types=1);
+<?php
+
+declare(strict_types=1);
 
 namespace Aeviiq\Enum\Tests;
 
 use Aeviiq\Enum\AbstractFlag;
+use Aeviiq\Enum\Exception\UnexpectedValueException;
 use PHPUnit\Framework\TestCase;
 
 final class AbstractFlagTest extends TestCase
 {
-    /**
-     * @var object|AbstractFlag
-     */
-    private $subject;
-
-    /**
-     * @dataProvider constructorThrowsExceptionOnInvalidValueDataProvider
-     */
-    public function testConstructorThrowsExceptionOnInvalidValue($value): void
-    {
-        $this->expectException(\UnexpectedValueException::class);
-        $this->createSubject($value);
-    }
-
-    public function constructorThrowsExceptionOnInvalidValueDataProvider(): array
+    public static function constructorThrowsExceptionOnInvalidValueDataProvider(): array
     {
         return [
             [4],
@@ -29,6 +18,54 @@ final class AbstractFlagTest extends TestCase
             [7],
             [12],
         ];
+    }
+
+    public static function constructorAllowsValidValuesAndCombinedBitValuesDataProvider(): array
+    {
+        return [
+            [1],
+            [2],
+            [3],
+            [8],
+            [11],
+        ];
+    }
+
+    public static function isValidSupportsCombinedBitValuesDataProvider(): array
+    {
+        return [
+            'Test with defined value' => [1, true,],
+            'Test with combined bit value' => [3, true],
+            'Test with missing value' => [4, false],
+            'Test with invalid combined bit value' => [5, false],
+        ];
+    }
+
+    public static function explodeDataProvider(): array
+    {
+        return [
+            'Test with defined value' => [2, [2]],
+            'Test with combined value' => [9, [1, 8]],
+            'Test with multiple combined values' => [11, [1, 2, 8]],
+        ];
+    }
+
+    public static function countDataProvider(): array
+    {
+        return [
+            'Test with 1 value' => [2, 1],
+            'Test with 2 values' => [9, 2],
+            'Test with 3 values' => [11, 3],
+        ];
+    }
+
+    /**
+     * @dataProvider constructorThrowsExceptionOnInvalidValueDataProvider
+     */
+    public function testConstructorThrowsExceptionOnInvalidValue($value): void
+    {
+        $this->expectException(UnexpectedValueException::class);
+        $this->createSubject($value);
     }
 
     /**
@@ -40,35 +77,15 @@ final class AbstractFlagTest extends TestCase
         static::assertEquals($value, $subject->getValue());
     }
 
-    public function constructorAllowsValidValuesAndCombinedBitValuesDataProvider(): array
-    {
-        return [
-            [1],
-            [2],
-            [3],
-            [8],
-            [11],
-        ];
-    }
 
     /**
      * @dataProvider isValidSupportsCombinedBitValuesDataProvider
      */
     public function testIsValidSupportsCombinedBitValues($value, bool $expected): void
     {
-        $result = $this->subject::isValid($value);
+        $subject = $this->createSubject(1);
+        $result = $subject::isValid($value);
         $expected ? static::assertTrue($result) : static::assertFalse($result);
-    }
-
-    public function isValidSupportsCombinedBitValuesDataProvider(): array
-    {
-        return [
-            'Test with defined value' => [1, true,],
-            'Test with combined bit value' => [3, true],
-            'Test with missing value' => [4, false],
-            'Test with invalid combined bit value' => [5, false],
-            'Test with invalid value' => ['invalid', false],
-        ];
     }
 
     public function testContainsChecksTheCorrectBits(): void
@@ -89,15 +106,6 @@ final class AbstractFlagTest extends TestCase
         self::assertEquals($subject->explode(), $expected);
     }
 
-    public function explodeDataProvider(): array
-    {
-        return [
-            'Test with defined value' => [2, [2]],
-            'Test with combined value' => [9, [1, 8]],
-            'Test with multiple combined values' => [11, [1, 2, 8]],
-        ];
-    }
-
     /**
      * @dataProvider countDataProvider
      */
@@ -107,20 +115,6 @@ final class AbstractFlagTest extends TestCase
         self::assertEquals($subject->count(), $expectedCount);
     }
 
-    public function countDataProvider(): array
-    {
-        return [
-            'Test with 1 value' => [2, 1],
-            'Test with 2 values' => [9, 2],
-            'Test with 3 values' => [11, 3],
-        ];
-    }
-
-    protected function setUp(): void
-    {
-        $this->subject = $this->createSubject(1);
-    }
-
     private function createSubject($flag): object
     {
         return new class($flag) extends AbstractFlag
@@ -128,11 +122,6 @@ final class AbstractFlagTest extends TestCase
             public const FLAG_1 = 1;
             public const FLAG_2 = 2;
             public const FLAG_3 = 8;
-
-            public function __construct($flag)
-            {
-                parent::__construct($flag);
-            }
         };
     }
 }
